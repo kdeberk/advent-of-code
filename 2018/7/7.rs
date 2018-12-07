49f6ed5dcabb1152b04c6a66bd2c93d7d73abc93
Kevin de Berk <kevin@dberk.nl>
2018-12-07 09:04:10 +0100

    Day 7

---
+++ 2018/7/7.rs
@@ -0,0 +1,99 @@
+
+#![feature(vec_remove_item)]
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
+
+fn read_requirements(filename: &str) -> HashMap<char, Vec<char>> {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(&file);
+    let mut requirements:HashMap<char, Vec<char>> = HashMap::new();
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let parts:Vec<&str> = line.split(' ').collect();
+
+        let before = parts[1].chars().next().unwrap();
+        requirements.entry(before).or_insert(vec![]);
+
+        let after = parts[7].chars().next().unwrap();
+        let mut required = requirements.entry(after).or_insert(vec![]);
+        required.push(before);
+    }
+
+    requirements
+}
+
+fn part_1(requirements: &HashMap<char, Vec<char>>) {
+    let mut done:Vec<char> = vec![];
+
+    while done.len() < requirements.len() {
+        for ch in "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars() {
+            if let Some(required) = requirements.get(&ch) {
+                if ! done.contains(&ch) && required.iter().all(|c| done.contains(c)) {
+                    done.push(ch);
+                    break
+                }
+            }
+        }
+    }
+    println!("{:?}", done.iter().collect::<String>());
+}
+
+const N_WORKERS:usize = 5;
+const TASK_COST:u32 = 60;
+
+#[derive(PartialEq, Eq)]
+struct Task {
+    ready_at: u32,
+    ch: char,
+}
+
+fn part_2(requirements: &HashMap<char, Vec<char>>) {
+    let mut time = 0;
+    let mut tasks:Vec<Task> = vec![];
+
+    let mut started_on:Vec<char> = vec![];
+    let mut done:Vec<char> = vec![];
+
+    while done.len() < requirements.len() {
+        let mut new_tasks = vec![];
+
+        for task in tasks {
+            if time >= task.ready_at {
+                done.push(task.ch);
+            } else {
+                new_tasks.push(task);
+            }
+        }
+        tasks = new_tasks;
+
+        let mut run = true;
+        while run && tasks.len() < N_WORKERS {
+            run = false;
+
+            for ch in "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars() {
+                if let Some(required) = requirements.get(&ch) {
+                    if ! started_on.contains(&ch) && required.iter().all(|c| done.contains(c)) {
+                        tasks.push( Task { ready_at: time + TASK_COST + (ch as u8 - 'A' as u8) as u32 + 1, ch: ch });
+                        started_on.push(ch);
+                        run = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        time += 1;
+    }
+
+
+    println!("{}", time - 1);
+}
+
+fn main() {
+    let requirements = read_requirements("input");
+    part_1(&requirements);
+    part_2(&requirements);
+}
