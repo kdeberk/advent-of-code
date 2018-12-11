f387c8e6abaf445d22cd668a12975993516439a4
Kevin de Berk <kevin@dberk.nl>
2018-12-11 21:47:42 +0100

    Day 11, changed algorithm and went from ~45s to ~0.33s

--- 2018/11/11.rs
+++ 2018/11/11.rs
@@ -2,23 +2,25 @@
 const SIZE:usize = 300;
 const SERIAL:usize = 8772;
 
-fn part_1(grid: &[[i8; SIZE]; SIZE]) {
+fn part_1(sums: &[[i32; SIZE]; SIZE]) {
     let mut max = std::i32::MIN;
     let mut max_coordinate = (0, 0);
 
-    for x in 0..SIZE - 2 {
-        for y in 0..SIZE - 2 {
-             let mut sum:i32 = 0;
+    for x in 0..SIZE - 3 {
+        for y in 0..SIZE - 3 {
+            // See: https://en.wikipedia.org/wiki/Summed-area_table
+            // the sum of all values within the rectangle bounded by the corners
+            //    A------B
+            //    |      |
+            //    |      |
+            //    C------D
+            // will be A+D-B-C
 
-            for xx in x..x + 3 {
-                for yy in y..y + 3 {
-                    sum += grid[yy][xx] as i32;
-                }
-            }
+            let sum = sums[y][x] + sums[y + 3][x + 3] - sums[y + 3][x] - sums[y][x + 3];
 
             if sum > max {
                 max = sum;
-                max_coordinate = (x, y);
+                max_coordinate = (x + 1, y + 1);
             }
         }
     }
@@ -26,26 +28,18 @@ fn part_1(grid: &[[i8; SIZE]; SIZE]) {
 }
 
 
-fn part_2(grid: &[[i8; SIZE]; SIZE]) {
+fn part_2(sums: &[[i32; SIZE]; SIZE]) {
     let mut max = std::i32::MIN;
     let mut max_coordinate = (0, 0, 0);
 
-    for x in 0..SIZE - 2 {
-        for y in 0..SIZE - 2 {
-            let mut grid_sum:i32 = 0;
+    for size in 0..SIZE {
+        for x in 0..SIZE - size {
+            for y in 0..SIZE - size {
+                let sum = sums[y][x] + sums[y + size][x + size] - sums[y + size][x] - sums[y][x + size];
 
-            for size in 0..(SIZE - std::cmp::max(x, y)) {
-                for xx in x..x + size {
-                    grid_sum += grid[y + size][xx] as i32;
-                }
-                for yy in y..y + size {
-                    grid_sum += grid[yy][x + size] as i32;
-                }
-                grid_sum += grid[y + size][x + size] as i32;
-
-                if grid_sum > max {
-                    max = grid_sum;
-                    max_coordinate = (x, y, size + 1);
+                if sum > max {
+                    max = sum;
+                    max_coordinate = (x + 1, y + 1, size);
                 }
             }
         }
@@ -55,20 +49,28 @@ fn part_2(grid: &[[i8; SIZE]; SIZE]) {
 
 
 fn main() {
-    let mut grid:[[i8; SIZE]; SIZE] = [[0; SIZE]; SIZE];
+    let mut sums:[[i32; SIZE]; SIZE] = [[0; SIZE]; SIZE];
+
+    // each cell in the sums contains the sums of all the cells that have a smaller x and/or smaller y value.
 
     for x in 0..SIZE {
         let rack_id = 10 + x;
 
         for y in 0..SIZE {
-            let mut power_level = rack_id * (rack_id * y + SERIAL);
+            let power_level = rack_id * (rack_id * y + SERIAL);
+            let fuel_cell = ((power_level % 1_000) / 100) as i32 - 5;
 
-            grid[y][x] = ((power_level % 1_000) / 100) as i8;
-            grid[y][x] -= 5;
+            if x > 0 && y > 0 {
+                // subtract sums[y - 1][x - 1] otherwise sums[y][x] will store twice that value through sums[y - 1][x] + sums[y][x - 1]
+                sums[y][x] = fuel_cell + sums[y - 1][x] + sums[y][x - 1] - sums[y - 1][x - 1]
+            } else if x > 0 {
+                sums[y][x] = fuel_cell + sums[y][x - 1]
+            } else if y > 0 {
+                sums[y][x] = fuel_cell + sums[y - 1][x]
+            }
         }
     }
 
-
-    part_1(&grid);
-    part_2(&grid);
+    part_1(&sums);
+    part_2(&sums);
 }
