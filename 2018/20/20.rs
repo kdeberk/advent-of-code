bd8eed161a7c9d4be5297de66ebe8b6c3145d1f6
Kevin de Berk <kevin@dberk.nl>
2018-12-20 18:41:47 +0100

    Day 20

--- 2018/20/20.rs
+++ 2018/20/20.rs
@@ -3,23 +3,45 @@ use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn construct_doors(regex: &str) -> HashMap<(isize, isize), HashSet<(isize, isize)>> {
-    let mut current_room = (0, 0);
-    let mut doors:HashMap<(isize, isize), HashSet<(isize, isize)>> = HashMap::new();
+struct Facility {
+    doors: HashMap<(isize, isize), HashSet<(isize, isize)>>,
+}
+
+impl Facility {
+    pub fn connect_rooms(&mut self, a: (isize, isize), b: (isize, isize)) {
+        self._connect(a, b);
+        self._connect(b, a);
+    }
+
+    pub fn neighboring_rooms(&self, room: &(isize, isize)) -> &HashSet<(isize, isize)> {
+        self.doors.get(&room).unwrap()
+    }
+
+    fn _connect(&mut self, a: (isize, isize), b: (isize, isize)) {
+        self.doors.entry(a)
+            .and_modify(|hs| { hs.insert(b); })
+            .or_insert({
+                let mut hs = HashSet::new();
+                hs.insert(b);
+                hs
+            });
+    }
+}
+
+fn construct_facility(filename: &str) -> Facility {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(&file);
+    let regex = reader.lines().next().unwrap().unwrap();
+
+    let mut facility = Facility { doors: HashMap::new() };
     let mut junctions:VecDeque<(isize, isize)> = VecDeque::new();
 
+    let mut current_room = (0, 0);
     for ch in regex.chars() {
         match ch {
-            '(' => {
-                junctions.push_back(current_room);
-            },
-            ')' => {
-                current_room = junctions.pop_back().unwrap();
-            },
-            '|' => {
-                current_room = *junctions.back().unwrap();
-            }
-            '$' | '^' => (),
+            '(' => { junctions.push_back(current_room); },
+            ')' => { current_room = junctions.pop_back().unwrap(); },
+            '|' => { current_room = *junctions.back().unwrap(); }
             'N' | 'W' | 'E' | 'S' => {
                 let next_room = match ch {
                     'N' => (current_room.0, current_room.1 + 1),
@@ -29,35 +51,23 @@ fn construct_doors(regex: &str) -> HashMap<(isize, isize), HashSet<(isize, isize
                     _ => panic!("Huh!?!?)"),
                 };
 
-                doors.entry(current_room)
-                    .and_modify(|hs| { hs.insert(next_room); })
-                    .or_insert({
-                        let mut hs = HashSet::new();
-                        hs.insert(next_room);
-                        hs
-                    });
-                doors.entry(next_room)
-                    .and_modify(|hs| { hs.insert(current_room); })
-                    .or_insert({
-                        let mut hs = HashSet::new();
-                        hs.insert(current_room);
-                        hs
-                    });
+                facility.connect_rooms(current_room, next_room);
                 current_room = next_room;
             },
+            '$' | '^' => (),
             _ => { panic!("unknown char {}", ch) },
         }
     }
-    doors
+    facility
 }
 
-fn draw_map(doors: &HashMap<(isize, isize), HashSet<(isize, isize)>>) {
+fn draw_map(facility: &Facility) {
     let mut min_x = std::isize::MAX;
     let mut max_x = std::isize::MIN;
     let mut min_y = std::isize::MAX;
     let mut max_y = std::isize::MIN;
 
-    for ((x, y), _) in doors.iter() {
+    for ((x, y), _) in facility.doors.iter() {
         if *x < min_x { min_x = *x }
         if max_x < *x { max_x = *x }
         if *y < min_y { min_y = *y }
@@ -81,7 +91,7 @@ fn draw_map(doors: &HashMap<(isize, isize), HashSet<(isize, isize)>>) {
                     let up_room = (room_x, room_y);
                     let down_room = (room_x, room_y + 1);
 
-                    if doors.contains_key(&up_room) && doors.get(&up_room).unwrap().contains(&down_room) {
+                    if facility.doors.contains_key(&up_room) && facility.doors.get(&up_room).unwrap().contains(&down_room) {
                         print!("-");
                     } else {
                         print!("#");
@@ -99,7 +109,7 @@ fn draw_map(doors: &HashMap<(isize, isize), HashSet<(isize, isize)>>) {
                     let left_room = (room_x, room_y);
                     let right_room = (room_x + 1, room_y);
                     
-                    if doors.contains_key(&left_room) && doors.get(&left_room).unwrap().contains(&right_room) {
+                    if facility.doors.contains_key(&left_room) && facility.doors.get(&left_room).unwrap().contains(&right_room) {
                         print!("|");
                     } else {
                         print!("#");
@@ -117,7 +127,7 @@ fn draw_map(doors: &HashMap<(isize, isize), HashSet<(isize, isize)>>) {
     }
 }
 
-fn find_max_distant_room(doors:&HashMap<(isize, isize), HashSet<(isize, isize)>>) -> usize {
+fn find_most_distant_room(facility: &Facility) -> usize {
     let mut seen:HashSet<(isize, isize)> = HashSet::new();
     let mut queue:VecDeque<((isize, isize), usize)> = VecDeque::new();
     let mut max_distance = std::usize::MIN;
@@ -128,25 +138,17 @@ fn find_max_distant_room(doors:&HashMap<(isize, isize), HashSet<(isize, isize)>>
         let (current, distance) = queue.pop_front().unwrap();
         max_distance = usize::max(distance, max_distance);
 
-        if seen.contains(&current) {
-            continue;
-        } else {
-            let other_rooms = doors.get(&current).unwrap();
-            for other_room in other_rooms.iter() {
-                if seen.contains(other_room) {
-                    continue
-                } else {
-                    queue.push_back((*other_room, distance + 1));
-                }
-            }
-
-            seen.insert(current);
+        if seen.contains(&current) { continue }
+        for other_room in facility.neighboring_rooms(&current).iter() {
+            if seen.contains(other_room) { continue }
+            queue.push_back((*other_room, distance + 1));
         }
+        seen.insert(current);
     }
     max_distance
 }
 
-fn count_rooms_with_greater_distance_than(doors: &HashMap<(isize, isize), HashSet<(isize, isize)>>, limit: usize) -> usize {
+fn count_rooms_with_greater_distance_than(facility: &Facility, limit: usize) -> usize {
     let mut count = 0;
     let mut seen:HashSet<(isize, isize)> = HashSet::new();
     let mut queue:VecDeque<((isize, isize), usize)> = VecDeque::new();
@@ -159,46 +161,34 @@ fn count_rooms_with_greater_distance_than(doors: &HashMap<(isize, isize), HashSe
             count += 1
         }
 
-        if seen.contains(&current) {
-            continue;
-        } else {
-            let other_rooms = doors.get(&current).unwrap();
-            for other_room in other_rooms.iter() {
-                if seen.contains(other_room) {
-                    continue
-                } else {
-                    queue.push_back((*other_room, distance + 1));
-                }
-            }
-
-            seen.insert(current);
+        if seen.contains(&current) { continue }
+        for other_room in facility.neighboring_rooms(&current).iter() {
+            if seen.contains(other_room) { continue }
+            queue.push_back((*other_room, distance + 1));
         }
+        seen.insert(current);
     }
     count
 }
 
-fn part_1(regex: &str) -> usize {
-    let doors = construct_doors(regex);
-    if env::var("DRAW").is_ok() { draw_map(&doors); }
-    find_max_distant_room(&doors)
+fn part_1(facility: &Facility) -> usize {
+    if env::var("DRAW").is_ok() { draw_map(&facility); }
+    find_most_distant_room(facility)
 }
 
-fn part_2(regex: &str) -> usize {
-    let doors = construct_doors(regex);
-    if env::var("DRAW").is_ok() { draw_map(&doors); }
-    count_rooms_with_greater_distance_than(&doors, 1000)
+fn part_2(facility: &Facility) -> usize {
+    if env::var("DRAW").is_ok() { draw_map(&facility); }
+    count_rooms_with_greater_distance_than(&facility, 1000)
 }
 
 fn main() {
-    assert!(3 == part_1("^WNE$"));
-    assert!(10 == part_1("^ENWWW(NEEE|SSE(EE|N))$"));
-    assert!(18 == part_1("^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$"));
-    assert!(23 == part_1("^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$"));
-    assert!(31 == part_1("^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$"));
+    assert!(3 == part_1(&construct_facility("1.test")));
+    assert!(10 == part_1(&construct_facility("2.test")));
+    assert!(18 == part_1(&construct_facility("3.test")));
+    assert!(23 == part_1(&construct_facility("4.test")));
+    assert!(31 == part_1(&construct_facility("5.test")));
 
-    let file = File::open("input").unwrap();
-    let reader = BufReader::new(&file);
-    let regex = reader.lines().next().unwrap().unwrap();
-    println!("{}", part_1(&regex));
-    println!("{}", part_2(&regex));
+    let facility = construct_facility("input");
+    println!("{}", part_1(&facility));
+    println!("{}", part_2(&facility));
 }
