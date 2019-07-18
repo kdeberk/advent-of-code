1f383c4209593b9b0a67a78cf78e4b03d9ec0c24
Kevin de Berk <kevin@dberk.nl>
2019-07-18 20:54:55 +0200

    2016. Day 2

--- 2016/constants.asm
+++ 2016/constants.asm
@@ -17,9 +17,16 @@
 %define THIRD_OF_FOUR_ARGS (ebp+12)
 %define FOURTH_OF_FOUR_ARGS (ebp+8)
 
+%define FIRST_OF_FIVE_ARGS (ebp+24)
+%define SECOND_OF_FIVE_ARGS (ebp+20)
+%define THIRD_OF_FIVE_ARGS (ebp+16)
+%define FOURTH_OF_FIVE_ARGS (ebp+12)
+%define FIFTH_OF_FIVE_ARGS (ebp+8)
+
 %define FIRST_VAR (ebp-4)
 %define SECOND_VAR (ebp-8)
 %define THIRD_VAR (ebp-12)
 %define FOURTH_VAR (ebp-16)
+%define FIFTH_VAR (ebp-20)
 
 %endif
