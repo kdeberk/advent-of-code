cef9ae0cd1b16661e32fc449a03e7edc9cb16da2
Kevin de Berk <kevin@dberk.nl>
2020-12-13 18:03:42 +0100

    2020. Speed up day 13 by sprinkling some type-hints

--- 2020/day13.clj
+++ 2020/day13.clj
@@ -4,8 +4,8 @@
   (let [[t bs] (.split input "\n")]
     [(Integer/parseInt t)
      (map (fn [[i b]]
-            [(biginteger (Integer/parseInt b))
-             (biginteger i)])
+            (let [p (biginteger (Integer/parseInt b))]
+              [p (.mod (biginteger i) p)]))
           (filter (fn [[_ b]]
                     (re-matches #"[0-9]+" b))
                   (map-indexed vector (.split bs ","))))]))
@@ -18,10 +18,10 @@
         [b wait] (apply min-key second waits)]
     (int (* b wait))))
 
-(defn extended-euclid [a b]
-  (loop [[s s'] [(biginteger 0) (biginteger 1)]
-         [r r'] [b a]
-         [t t'] [(biginteger 1) (biginteger 0)]]
+(defn extended-euclid [^BigInteger a ^BigInteger b]
+  (loop [[^BigInteger s ^BigInteger s'] [(biginteger 0) (biginteger 1)]
+         [^BigInteger r ^BigInteger r'] [b a]
+         [^BigInteger t ^BigInteger t'] [(biginteger 1) (biginteger 0)]]
     (if (not (zero? r))
       (let [q (.divide r' r)]
         (recur [(.subtract s' (.multiply q s)) s]
@@ -29,11 +29,12 @@
                [(.subtract t' (.multiply q t)) t]))
       [r' s' t'])))
 
-(defn solve-modulo-equations [xs]
+(defn ^BigInteger solve-modulo-equations [xs]
   ;; From: https://homepages.math.uic.edu/~leon/mcs425-s08/handouts/chinese_remainder.pdf
-  (let [m (reduce #(.multiply %1 %2) (map first xs))]
-    (reduce #(.add %1 %2)
-            (map (fn [[p a]]
+  (let [^BigInteger m (reduce (fn [^BigInteger a ^BigInteger b] (.multiply a b))
+                              (map first xs))]
+    (reduce (fn [^BigInteger a ^BigInteger b] (.add a b))
+            (map (fn [[^BigInteger p ^BigInteger a]]
                    (let [z (.divide m p)
                          [_ _ y] (extended-euclid p z)]
                      (.multiply (.multiply a y) z)))
@@ -41,7 +42,7 @@
 
 ;; I have no idea why I need the largest negative number instead of
 ;; the smallest positive number.
-(defn largest-negative-multiple [x m]
+(defn largest-negative-multiple [^BigInteger x ^BigInteger m]
   (if (< 0 x)
     (loop [x x]
       (if (< 0 x)
@@ -54,6 +55,6 @@
 
 (defn part2 [input]
   (let [[_ buses] (parse-input input)
-        m (reduce (fn [x y] (.multiply x y)) (map first buses))
+        m (reduce (fn [^BigInteger x ^BigInteger y] (.multiply x y)) (map first buses))
         x (solve-modulo-equations buses)]
     (.abs (largest-negative-multiple x m))))
