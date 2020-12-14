eefcea0a9dcde09474aec122a2c2e644346ce077
Kevin de Berk <kevin@dberk.nl>
2020-12-14 19:58:11 +0100

    2020. Reduce ❤︎

--- 2020/day14.clj
+++ 2020/day14.clj
@@ -5,57 +5,49 @@
   (map (fn [line]
          (let [[dst v] (.split line " = ")]
            (if (= dst "mask")
-             [:mask v]
+             [:mask (map-indexed vector (reverse v))]
              (let [[_ m] (re-matches #"mem\[(\d+)\]" dst)]
                [(Integer/parseInt m)
-                (str/join "" (let [bits (Integer/toBinaryString (Integer/parseInt v))]
-                               (concat (map (fn [_] \0) (range 0 (- 36 (count bits))))
-                                       bits)))]))))
+                (Integer/parseInt v)]))))
        (.split input "\n")))
 
-(defn apply-mask [val mask]
-  (str/join "" (map (fn [v m]
-                      (case m \0 0 \1 1 \X v))
-                    val
-                    mask)))
-
 (defn part1 [input]
-  (loop [instructions (parse-lines input)
-         mem {}
-         mask nil]
-    (if (empty? instructions)
-      (apply + (map #(Long/parseLong % 2) (vals mem)))
-      (let [[dst v] (first instructions)]
-        (if (= dst :mask)
-          (recur (rest instructions) mem v)
-          (recur (rest instructions)
-                 (assoc mem dst (apply-mask v mask))
-                 mask))))))
-
-(defn explode-mask [val mask]
-  (loop [acc [val] mask mask]
-    (if (empty? mask)
-      acc
-      (case (first mask)
-        \0 (recur acc (rest mask))
-        \1 (recur (map #(bit-set % (dec (count mask))) acc)
-                  (rest mask))
-        \X (recur (concat (map #(bit-clear % (dec (count mask))) acc)
-                          (map #(bit-set % (dec (count mask))) acc))
-                  (rest mask))))))
+  (letfn [(apply-mask [val mask]
+            (reduce (fn [v [i m]]
+                      (case m
+                        \0 (bit-clear v i)
+                        \1 (bit-set   v i)
+                        \X v))
+                    val
+                    mask))]
+    (let [instructions (parse-lines input)
+          [final _] (reduce (fn [[mem mask] [dst val]]
+                              (if (= dst :mask)
+                                [mem val]
+                                [(assoc mem dst (apply-mask val mask)) mask]))
+                            [{} nil]
+                            instructions)]
+      (apply + (vals final)))))
 
 (defn part2 [input]
-  (loop [instructions (parse-lines input)
-         mem {}
-         mask nil]
-    (if (empty? instructions)
-      (apply + (map #(Long/parseLong % 2) (vals mem)))
-      (let [[dst v] (first instructions)]
-        (if (= dst :mask)
-          (recur (rest instructions) mem v)
-          (recur (rest instructions)
-                 (loop [mem mem dsts (explode-mask dst mask)]
-                   (if (empty? dsts)
-                     mem
-                     (recur (assoc mem (first dsts) v) (rest dsts))))
-                 mask))))))
+  (letfn [(explode-masks [addr mask]
+            (reduce (fn [addrs [i m]]
+                      (case m
+                        \0 addrs
+                        \1 (map #(bit-set % i) addrs)
+                        \X (concat (map #(bit-set % i) addrs)
+                                   (map #(bit-clear % i) addrs))))
+                    [addr]
+                    mask))]
+    (let [instructions (parse-lines input)
+          [final _] (reduce (fn [[mem mask] [dst val]]
+                              (if (= dst :mask)
+                                [mem val]
+                                [(reduce (fn [mem addr]
+                                           (assoc mem addr val))
+                                         mem
+                                         (explode-masks dst mask))
+                                 mask]))
+                            [{} nil]
+                            instructions)]
+      (apply + (vals final)))))
