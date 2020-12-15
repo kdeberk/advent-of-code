6004c9b1e584b74acf3b0fc2db0bdb8693d17ec4
Kevin de Berk <kevin@dberk.nl>
2020-12-15 19:30:48 +0100

    2020. Day 15 a bit faster

--- 2020/day15.clj
+++ 2020/day15.clj
@@ -1,19 +1,20 @@
 (ns aoc.2020.day15)
 
 (defn parse-starting-numbers [input]
-  (map #(Integer/parseInt %) (.split input ",")))
+  (map #(Long/parseLong %) (.split input ",")))
 
 (defn memory-game [starting nth]
-  (loop [turn (inc (count starting))
-         last (last starting)
-         mem  (into {} (map-indexed (fn [x y] [y (inc x)]) starting))]
-    (let [next (if-let [idx (mem last)]
-                 (- (dec turn) idx)
-                 0)]
-      (if (= turn nth)
-        next
-        (recur (inc turn) next (assoc mem last (dec turn)))))))
-
+  (let [mem (java.util.HashMap. (into {} (map-indexed (fn [x y] [y (inc x)]) starting)))]
+    (loop [turn (inc (count starting))
+           last (last starting)]
+      (let [next (if-let [idx (.get mem last)]
+                   (- (dec turn) idx)
+                   0)]
+        (if (= nth turn)
+          next
+          (do
+            (.put mem last (dec turn))
+            (recur (inc turn) next)))))))
 
 (defn part1 [input]
   (memory-game (parse-starting-numbers input) 2020))
