8ab096813c047295106fe4fe50b1004160662ae7
Kevin de Berk <kevin@dberk.nl>
2021-12-01 07:46:31 +0100

    2021. Day 1

---
+++ 2021/day1.lisp
@@ -0,0 +1,7 @@
+(defun part1 (input)
+  (loop for (a b) on input
+        count (and b (< a b))))
+
+(defun part2 (input)
+  (part1 (loop for (a b c) on input
+               if (and b c) collect (+ a b c))))
