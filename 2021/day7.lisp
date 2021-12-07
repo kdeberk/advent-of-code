9823cc7792ae6a3f261d925cda6a174f72cf429f
Kevin de Berk <kevin@dberk.nl>
2021-12-07 21:53:35 +0100

    Day 7: Linear & near linear solution

--- 2021/day7.lisp
+++ 2021/day7.lisp
@@ -3,31 +3,28 @@
 (defvar *test-input* (utils:read-numbers "day7_test.txt"))
 (defvar *input* (utils:read-numbers "day7.txt"))
 
-(defun read-crabs (numbers)
-  (let ((pairs))
-    (dolist (n numbers)
-      (let ((p (assoc n pairs)))
-        (if p
-            (incf (cdr p))
-            (push (cons n 1) pairs))))
-    (sort pairs #'< :key #'car)))
-
 (defun linear-dist (x y)
   (abs (- x y)))
 
 (defun triangular-dist (x y)
-  (let ((d (linear-dist x y)))
-    (abs (* d (1+ d) 1/2))))
+  (let ((d (abs (- x y))))
+    (* d (1+ d) 1/2)))
 
-(defun needed-fuel (crabs distfn)
-  (let ((start (caar crabs))
-        (end (caar (last crabs))))
-    (loop for x from start to end
-          minimizing (loop for crab in crabs
-                           sum (* (cdr crab) (funcall distfn x (car crab)))))))
+(defun calculate-required-fuel (middle crabs distfn)
+  (loop for crab in crabs
+        sum (funcall distfn middle crab)))
 
 (defun part1 (input)
-  (needed-fuel (read-crabs input) #'linear-dist))
+  (let* ((crabs (sort (apply #'list input) #'<))
+         (middle (nth (round (/ (length crabs) 2)) crabs)))
+    (calculate-required-fuel middle crabs #'linear-dist)))
 
 (defun part2 (input)
-  (needed-fuel (read-crabs input) #'triangular-dist))
+  (let ((middle (/ (apply #'+ input) (length input))))
+    (apply #'min (mapcar (lambda (mid)
+                           (calculate-required-fuel mid input #'triangular-dist))
+                         (list (floor middle) (ceiling middle))))))
+
+(define-test day7
+  (is = 37 (part1 *test-input*))
+  (is = 168 (part2 *test-input*)))
